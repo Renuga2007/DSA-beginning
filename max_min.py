7450a63034aa901ef0f2bd61d71632fceb253429
Renuga2007 <renuga78718@gmail.com>
2025-12-06 22:39:43 +0530

    max_min.py

---
+++ max_min.py
@@ -0,0 +1,9 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> numbers =  [3, 8, 2, 9, 12, 7]
+>>> print("using Built-in Functions:")
+using Built-in Functions:
+>>> print("Maximum =", max(numbers))
+Maximum = 12
+>>> print("Minimum =", min(numbers))
+Minimum = 2
